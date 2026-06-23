89a053604c7df2929be58dc58143a38ba4098f11
Darragh Drennan (Tallaght TC Student) <d.drennan25@ddletbtc.ie>
2026-06-23 11:01:22 +0100

    Add comments to explain clearly

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,7 +1,18 @@
+# Import the tkinter GUI library.
 import tkinter as tk
+
+# Importing tkinter widgets.
 from tkinter import ttk, messagebox
 
+# Expense Tracker class.
 class ExpenseTracker:
+    
+    # Constructor methods that runs automatically when the application starts. 
     def __init__(self, root):
+
+        # Store the main application.
         self.root = root
         self.root.title("Expense Tracker")
+
+        # Empty list for storing transactions. 
+        self.transactions = []
