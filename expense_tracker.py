b0d1deddb026968cd692ced91d7b4d3bfc837ff7
Darragh Drennan (Tallaght TC Student) <d.drennan25@ddletbtc.ie>
2026-06-23 09:38:15 +0100

    Add Expense Tracker class and title

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,2 +1,7 @@
 import tkinter as tk
 from tkinter import ttk, messagebox
+
+class ExpenseTracker:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Expense Tracker")
