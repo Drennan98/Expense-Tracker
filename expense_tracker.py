8376332ccbe454c30ede1f18e9040e0ed38bab2e
Darragh Drennan (Tallaght TC Student) <d.drennan25@ddletbtc.ie>
2026-06-23 11:42:34 +0100

    Add entry fields for GUI

--- expense_tracker.py
+++ expense_tracker.py
@@ -17,7 +17,24 @@ class ExpenseTracker:
         # Empty list for storing transactions. 
         self.transactions = []
 
+        # Labels for GUI.
         tk.Label(root, text="Description").grid(row=0, column=0)
         tk.Label(root, text="Amount").grid(row=1, column=0)
         tk.Label(root, text="Category").grid(row=2, column=0)
         tk.Label(root, text="Type").grid(row=3, column=0)
+
+        # Entry fields
+        self.description_entry = tk.Entry(root)
+        self.description_entry.grid(row=1, column=1)
+
+        self.amount_entry = tk.Entry(root)
+        self.amount_entry.grid(row=1, column=1)
+
+        self.category_entry = tk.Entry(root)
+        self.category_entry.grid(row=2, column=1)
+
+        self.type_combo = ttk.Combobox(
+            root,
+            values=["Income", "Expense"]
+        )
+        self.type_combo.grid(row=3, column=1)
